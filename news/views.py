3fb8278aa5d48f6d1acf0035aa4d3c08830932fe
Qobiljon <madrahimovq@gmail.com>
2024-12-12 18:05:16 +0500

    6-kun

--- news/views.py
+++ news/views.py
@@ -8,7 +8,29 @@ from .models import Post, Category
 
 def index(request: WSGIRequest):
     posts = Post.objects.all()
+    categories = Category.objects.all()
     context = {
-        "posts": posts
+        "posts": posts,
+        'categories': categories
     }
     return render(request, "index.html", context)
+
+
+def posts_by_category(request, category_id):
+    categories = Category.objects.all()
+    posts = Post.objects.filter(category_id=category_id)
+
+    context = {
+        "categories": categories,
+        'posts': posts
+    }
+    return render(request, 'index.html', context)
+
+
+def post_detail(request, post_id):
+    post = Post.objects.get(pk=post_id)
+
+    context = {
+        "post": post
+    }
+    return render(request, 'detail.html', context)
